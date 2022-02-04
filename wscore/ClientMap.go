d19f2f14b5433f68af4ebbc4fe1ec9e8954b633f
WangYiwei-oss <wangyiwei@seu.edu.cn>
2022-02-04 19:53:57 +0800

    websocket添加发送策略验证

--- wscore/ClientMap.go
+++ wscore/ClientMap.go
@@ -64,14 +64,17 @@ func (c *ClientMap) Delete(conn *websocket.Conn) {
 	return
 }
 
+//SendAllClass 对所有class类别且满足自定义的策略的客户端发送消息
 func (c *ClientMap) SendAllClass(class string, v interface{}) {
 	if conns, ok := c.data[class]; ok {
 		for _, wsClient := range conns {
-			err := wsClient.conn.WriteJSON(v)
-			if err != nil {
-				c.Delete(wsClient.conn)
-				log.Println("WebSocket Core:", err, ", Will Delete Conn")
-				continue
+			if wsClient.SendStrategy(wsClient.Label) { //策略生效才会发送
+				err := wsClient.conn.WriteJSON(v)
+				if err != nil {
+					c.Delete(wsClient.conn)
+					log.Println("WebSocket Core:", err, ", Will Delete Conn")
+					continue
+				}
 			}
 		}
 	} else {
