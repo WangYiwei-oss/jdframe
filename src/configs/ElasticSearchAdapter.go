3f961efb97b89201933d6133586e826fc1567b98
WangYiwei-oss <wangyiwei@seu.edu.cn>
2022-02-22 00:02:13 +0800

    添加elastic search的配置项

--- src/configs/ElasticSearchAdapter.go
+++ src/configs/ElasticSearchAdapter.go
@@ -1,8 +1,11 @@
 package configs
 
 import (
+	"fmt"
 	"github.com/WangYiwei-oss/jdframe/src/configinjector"
+	"github.com/WangYiwei-oss/jdframe/src/configparser"
 	"github.com/olivere/elastic/v7"
+	"log"
 	"sync"
 )
 
@@ -21,10 +24,16 @@ var es *elastic.Client
 func NewEsAdapter() *EsAdapter {
 	var err error
 	doOnceGorm.Do(func() {
+		config := configparser.GlobalSettings["ELASTICSEARCH_CONFIG"].(map[string]interface{})
 		es, err = elastic.NewClient(
-			elastic.SetURL("http://192.168.83.150:31000"),
+			elastic.SetURL(fmt.Sprintf("http://%s:%s", config["IP"].(string), config["PORT"].(string))),
 			elastic.SetSniff(false),
 		)
+		if err != nil {
+			log.Fatalln("连接ElasticSearch失败", err)
+		} else {
+			log.Println("连接ElasticSearch成功")
+		}
 	})
 	return &EsAdapter{
 		Client: es,
